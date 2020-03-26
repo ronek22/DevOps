193ccb55246f8f13a28401a777fa6cd36e5cdccf
ronek22 <jronek3010@gmail.com>
2020-03-26 16:25:00 +0100

    Added -it to web service to fix react bug

--- mydockerfrontend/src/App.js
+++ mydockerfrontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World with !
+          Hello World with Covid-19!
         </p>
         <a
           className="App-link"
