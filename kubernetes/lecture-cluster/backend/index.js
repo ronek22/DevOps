be048b9a0fa664e53ef9dc676e5d1522b82e5d36
ronek22 <jronek3010@gmail.com>
2020-05-07 17:35:01 +0200

    Working on running-calculator kubernetes solution

--- kubernetes/lecture-cluster/backend/index.js
+++ kubernetes/lecture-cluster/backend/index.js
@@ -6,8 +6,8 @@ const app = express();
 const redis = require('redis');
 
 const redisClient = redis.createClient({
-    host: "myredisservice",
-    port: 6379,
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
     retry_strategy: () => 1000
 });
 
