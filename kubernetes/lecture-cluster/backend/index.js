5d3f913e5efbabd9ff9538bd543e0cf64ec6a2c7
ronek22 <jronek3010@gmail.com>
2020-05-14 16:03:29 +0200

    Postgres, secrets, configMaps

--- kubernetes/lecture-cluster/backend/index.js
+++ kubernetes/lecture-cluster/backend/index.js
@@ -1,21 +1,24 @@
 const {v4: uuidv4} = require('uuid');
 const express = require("express");
+const keys = require("./keys");
 
 const app = express();
 
 const redis = require('redis');
 
 const redisClient = redis.createClient({
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
+    host: keys.redisHost,
+    port: keys.redisPort,
     retry_strategy: () => 1000
 });
 
 const appId = uuidv4();
 const appPort = 5000;
 
+console.log(keys);
+
 app.get('/', (req,res) => {
-    res.send(`[${appId}] Hello from backend app`);
+    res.send(`[${appId}] ${keys.initMessage}`);
 })
 
 app.listen(appPort, err => {
